c971e203195775ff813492e957621ef05cd8f393
Sirikye Brian <bryanmuloni@gmail.com>
2019-07-17 18:19:44 +0300

    Intent Service Demo

---
+++ app/src/main/java/com/sirikyebrian/androidservices/MyIntentService.java
@@ -0,0 +1,51 @@
+package com.sirikyebrian.androidservices;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+
+/**
+ * Created by Sirikye Brian on 7/17/2019.
+ * bryanmuloni@gmail.com
+ */
+public class MyIntentService extends IntentService {
+    public static final String TAG = MyIntentService.class.getSimpleName();
+    public static final String EXTRA_SLEEP_TIME = "Sleep Time";
+
+    public MyIntentService() {
+        super("MyWorkerThread");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate, Thread name " + Thread.currentThread().getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent, Thread name " + Thread.currentThread().getName());
+
+        int sleepTime = intent.getIntExtra(EXTRA_SLEEP_TIME, 1);
+
+        int ctr = 1;
+
+//            Dummy long operation
+        while (ctr <= sleepTime) {
+            Log.i(TAG, "Counter is now " + ctr);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            ctr++;
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy, Thread name " + Thread.currentThread().getName());
+    }
+}
